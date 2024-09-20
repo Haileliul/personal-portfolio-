15cd2b64435c6171b90ce924b92a9d933a5773a6
haile <hailetechlover@gmail.com>
2024-09-20 10:01:21 +0300

    Update has been made to bottom contact

--- src/components/bottombar.jsx
+++ src/components/bottombar.jsx
@@ -7,7 +7,10 @@ import {
   BsTwitter,
 } from "react-icons/bs";
 
+import Data from "../utils/data";
+
 const Footerbar = () => {
+  const Contact = Data.Contacts.Social_Media;
   return (
     // <!--Footer container-->
     <footer className="flex flex-col items-center bg-zinc-50 text-center text-surface dark:bg-neutral-700 dark:text-white">
@@ -15,7 +18,7 @@ const Footerbar = () => {
         {/* <!-- Social media icons container --> */}
         <div className="mb-6 flex justify-center space-x-2">
           <a
-            href="#!"
+            href={Contact.Gmail}
             type="button"
             className="rounded-full bg-[#3b5998] p-3 uppercase leading-normal text-white shadow-dark-3 shadow-black/30 transition duration-150 ease-in-out hover:shadow-dark-1 focus:shadow-dark-1 focus:outline-none focus:ring-0 active:shadow-1 dark:text-white"
             data-twe-ripple-init
@@ -32,11 +35,11 @@ const Footerbar = () => {
               </svg>
             </span>
           </a>
-
           <a
-            href="#!"
+            href={Contact.Telegram}
+            target="_blank"
             type="button"
-            className="rounded-full bg-[#55acee] p-3 uppercase leading-normal text-white shadow-dark-3 shadow-black/30 transition duration-150 ease-in-out hover:shadow-dark-1 focus:shadow-dark-1 focus:outline-none focus:ring-0 active:shadow-1 dark:text-white"
+            className="rounded-full bg-[#0088cc] p-3 uppercase leading-normal text-white shadow-dark-3 shadow-black/30 transition duration-150 ease-in-out hover:shadow-dark-1 focus:shadow-dark-1 focus:outline-none focus:ring-0 active:shadow-1 dark:text-white"
             data-twe-ripple-init
             data-twe-ripple-color="light"
           >
@@ -44,16 +47,16 @@ const Footerbar = () => {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
-                viewBox="0 0 512 512"
+                viewBox="0 0 496 512"
               >
-                {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. --> */}
-                <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                <path d="M248 8C111.2 8 0 119.3 0 256s111.2 248 248 248 248-111.3 248-248S384.8 8 248 8zm113.4 169.1L345 371.7c-2.4 10.6-9.7 13.3-19.6 8.3l-54.3-40.1-26.2 25.2c-2.9 2.9-5.3 5.3-10.8 5.3l3.9-55.2 100.7-90.9c4.4-3.9-.9-6-6.9-2.1L214 296l-52.8-16.5c-10.7-3.3-10.9-10.7 2.3-15.8l206.5-79.3c9.6-3.9 18.1 2.1 15.4 14.8z" />
               </svg>
             </span>
           </a>
 
           <a
-            href="#!"
+            href={`mailto:${Contact.Gmail}?subject=Your%20Subject&body=Your%20Message`}
+            target="_blank"
             type="button"
             className="rounded-full bg-[#dd4b39] p-3 uppercase leading-normal text-white shadow-dark-3 shadow-black/30 transition duration-150 ease-in-out hover:shadow-dark-1 focus:shadow-dark-1 focus:outline-none focus:ring-0 active:shadow-1 dark:text-white"
             data-twe-ripple-init
@@ -72,26 +75,8 @@ const Footerbar = () => {
           </a>
 
           <a
-            href="#!"
-            type="button"
-            className="rounded-full bg-[#ac2bac] p-3 uppercase leading-normal text-white shadow-dark-3 shadow-black/30 transition duration-150 ease-in-out hover:shadow-dark-1 focus:shadow-dark-1 focus:outline-none focus:ring-0 active:shadow-1 dark:text-white"
-            data-twe-ripple-init
-            data-twe-ripple-color="light"
-          >
-            <span className="mx-auto [&>svg]:h-5 [&>svg]:w-5">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 448 512"
-              >
-                {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. --> */}
-                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-              </svg>
-            </span>
-          </a>
-
-          <a
-            href="#!"
+            href={Contact.linkedIn}
+            target="_blank"
             type="button"
             className="rounded-full bg-[#0082ca] p-3 uppercase leading-normal text-white shadow-dark-3 shadow-black/30 transition duration-150 ease-in-out hover:shadow-dark-1 focus:shadow-dark-1 focus:outline-none focus:ring-0 active:shadow-1 dark:text-white"
             data-twe-ripple-init
@@ -110,7 +95,8 @@ const Footerbar = () => {
           </a>
 
           <a
-            href="#!"
+            href={Contact.github}
+            target="_blank"
             type="button"
             className="rounded-full bg-[#333333] p-3 uppercase leading-normal text-white shadow-dark-3 shadow-black/30 transition duration-150 ease-in-out hover:shadow-dark-1 focus:shadow-dark-1 focus:outline-none focus:ring-0 active:shadow-1 dark:text-white"
             data-twe-ripple-init
